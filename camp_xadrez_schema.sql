21b79ba4a2229fde93af759912d7bc1c44628ee3
isael-lucas <isael.lucas@usp.br>
2023-06-12 22:58:49 -0300

    insercao de dummy no sql

--- camp_xadrez_schema.sql
+++ camp_xadrez_schema.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE xadrez;
+USE xadrez;
+
 SET foreign_key_checks = 0;
 DROP TABLE IF EXISTS `Hotel`;
 DROP TABLE IF EXISTS `Participante`;
@@ -104,3 +107,92 @@ CREATE TABLE `Pais` (
   `NumClubes` integer NOT NULL,
   PRIMARY KEY (`NumPais`)
 ) DEFAULT CHARSET=utf8;
+
+-- Inserindo dados nas tabelas Hotel, Participante, Salao, Acomoda, Jogador, CampPart, MedioSal, Jogo, Movimento, Joga e Pais
+INSERT INTO Hotel (NomeHotel, EndHotel, Telefone)
+VALUES 
+    ('Hotel A', 'Endereço A', '1234567890'),
+    ('Hotel B', 'Endereço B', '9876543210'),
+    ('Hotel C', 'Endereço C', '5555555555'),
+    ('Hotel D', 'Endereço D', '4444444444'),
+    ('Hotel E', 'Endereço E', '5555555555');
+
+INSERT INTO Participante (NumAssoc, NomeAssoc, Endereco, TelContato, CodPais, TipoPart)
+VALUES 
+    (1, 'Participante 1', 'Endereço 1', '1111111111', 'BR', 'Tipo 1'),
+    (2, 'Participante 2', 'Endereço 2', '2222222222', 'US', 'Tipo 2'),
+    (3, 'Participante 3', 'Endereço 3', '3333333333', 'ES', 'Tipo 3'),
+    (4, 'Participante 4', 'Endereço 4', '4444444444', 'UK', 'Tipo 4'),
+    (5, 'Participante 5', 'Endereço 5', '5555555555', 'FR', 'Tipo 5');
+
+INSERT INTO Salao (IdSal, NomeHotel, capacidade)
+VALUES 
+    (1, 'Hotel A', 100),
+    (2, 'Hotel B', 150),
+    (3, 'Hotel C', 200),
+    (4, 'Hotel D', 120),
+    (5, 'Hotel E', 180);
+
+INSERT INTO Acomoda (NomeHotel, NumAssoc, DataSaida, DataEntrada)
+VALUES 
+    ('Hotel A', 1, '2023-06-15', '2023-06-10'),
+    ('Hotel B', 2, '2023-06-16', '2023-06-11'),
+    ('Hotel C', 3, '2023-06-17', '2023-06-12'),
+    ('Hotel D', 4, '2023-06-18', '2023-06-13'),
+    ('Hotel E', 5, '2023-06-19', '2023-06-14');
+
+INSERT INTO Jogador (NumAssoc, Nivel)
+VALUES 
+    (1, 'Nível 1'),
+    (2, 'Nível 2'),
+    (3, 'Nível 3'),
+    (4, 'Nível 4'),
+    (5, 'Nível 5');
+
+INSERT INTO CampPart (NumAssoc, NomeCamp, TipoParticip)
+VALUES 
+    (1, 'Campeonato 1', 'Tipo 1'),
+    (2, 'Campeonato 2', 'Tipo 2'),
+    (3, 'Campeonato 3', 'Tipo 3'),
+    (4, 'Campeonato 4', 'Tipo 4'),
+    (5, 'Campeonato 5', 'Tipo 5');
+
+INSERT INTO MedioSal (IdSal, Medio)
+VALUES 
+    (1, 'Médio 1'),
+    (2, 'Médio 2'),
+    (3, 'Médio 3'),
+    (4, 'Médio 4'),
+    (5, 'Médio 5');
+
+INSERT INTO Jogo (CodJogo, NumArb, IdSal, EntrVend, DiaJorn, MesJorn, AnoJorn)
+VALUES 
+    (1, 1, 1, 100, 1, 6, 2023),
+    (2, 2, 2, 200, 2, 6, 2023),
+    (3, 3, 3, 300, 3, 6, 2023),
+    (4, 4, 4, 400, 4, 6, 2023),
+    (5, 5, 5, 500, 5, 6, 2023);
+
+INSERT INTO Movimento (CodJogo, IdMov, Jogada, Comentario)
+VALUES 
+    (1, 1, 'Jogada 1', 'Comentário 1'),
+    (2, 2, 'Jogada 2', 'Comentário 2'),
+    (3, 3, 'Jogada 3', 'Comentário 3'),
+    (4, 4, 'Jogada 4', 'Comentário 4'),
+    (5, 5, 'Jogada 5', 'Comentário 5');
+
+INSERT INTO Joga (CodJogo, NumJogador, Cor)
+VALUES 
+    (1, 1, 'Branco'),
+    (2, 2, 'Preto'),
+    (3, 3, 'Branco'),
+    (4, 4, 'Preto'),
+    (5, 5, 'Branco');
+
+INSERT INTO Pais (NumPais, NomePais, NumClubes)
+VALUES 
+    ('BR', 'Brasil', 10),
+    ('US', 'Estados Unidos', 15),
+    ('ES', 'Espanha', 8),
+    ('UK', 'Reino Unido', 12),
+    ('FR', 'França', 6);
