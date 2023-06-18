fb6131f5d27033f963b04e539d8dcfc63b17510f
Jhonatan <jhow_72@usp.br>
2023-06-18 20:20:20 -0300

    retirada de mencoes ao museu

--- CampeonatoXadrez/src/app/app-routing.module.ts
+++ CampeonatoXadrez/src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CollectionsComponent } from './pages/collections/collections.component';
 import { GraphicsComponent } from './pages/graphics/graphics.component';
 import { HomeComponent } from './pages/home/home.component';
-import { PurchasesComponent } from './pages/purchases/purchases.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'colections', component: CollectionsComponent },
-  { path: 'purchases', component:  PurchasesComponent},
   { path: 'graphics', component: GraphicsComponent },
   { path: '**', redirectTo: 'home' },
 
